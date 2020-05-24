e8c36cb4f8f0d1641b8a129bdfe2927cb03cf578
midbel <brun.nicolas21@gmail.com>
2020-05-24 09:21:58 +0200

    update UI + set font to app

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFont font;
+    font.setFamily(font.defaultFamily());
+    font.setPointSize(9);
+    a.setFont(font);
+
     qRegisterMetaType<Path>();
 
     QLocale::setDefault(QLocale(QLocale::English, QLocale::UnitedStates));
